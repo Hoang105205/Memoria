fb5edebfc7c6624e3ddeafa32e7dcda87050f06e
Hoang105205 <huyhoangluu105@gmail.com>
2026-02-11 16:56:40 +0700

    update: login page UI & sign up page UI

--- app/src/main/java/com/example/memoria/ui/auth/SignUpActivity.java
+++ app/src/main/java/com/example/memoria/ui/auth/SignUpActivity.java
@@ -12,7 +12,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private EditText etEmail, etPassword;
+    private EditText etEmail, etPassword, etConfirmPassword;
     private FirebaseAuth mAuth;
 
     @Override
@@ -24,20 +24,31 @@ public class SignUpActivity extends AppCompatActivity {
 
         etEmail = findViewById(R.id.auth_signup_et_email);
         etPassword = findViewById(R.id.auth_signup_et_password);
+        etConfirmPassword = findViewById(R.id.auth_signup_et_confirm_password);
         Button btnSignUp = findViewById(R.id.auth_signup_btn_signup);
         TextView tvTabLogin = findViewById(R.id.auth_signup_tv_tab_login);
 
         btnSignUp.setOnClickListener(v -> {
             String email = etEmail.getText().toString().trim();
             String pass = etPassword.getText().toString().trim();
-            if(email.isEmpty() || pass.isEmpty()) return;
+            String confirmPass = etConfirmPassword.getText().toString().trim();
+            
+            if(email.isEmpty() || pass.isEmpty() || confirmPass.isEmpty()) {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            
+            if(!pass.equals(confirmPass)) {
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
             mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(task -> {
                 if(task.isSuccessful()) {
                     startActivity(new Intent(this, MainActivity.class));
                     finish();
                 } else {
-                    Toast.makeText(this, "Sign Up Failed", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Sign Up Failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
         });
