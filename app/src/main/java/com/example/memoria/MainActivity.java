8c89415a94a0817a4defdc6ad54da16e411ae166
MP <nbmphuong2005@gmail.com>
2026-02-23 14:59:04 +0700

    feat: navigation bar ui

--- app/src/main/java/com/example/memoria/MainActivity.java
+++ app/src/main/java/com/example/memoria/MainActivity.java
@@ -7,18 +7,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        BottomNavigationView bottomNavigationView =
+                findViewById(R.id.bottomNavigationView);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.fragmentContainerView);
+
+        NavController navController =
+                navHostFragment.getNavController();
+
+        // Khai báo các top-level destinations
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(
+                        R.id.home_graph,
+                        R.id.search_graph,
+                        R.id.library_graph,
+                        R.id.profile_graph
+                ).build();
+
+
+        // Kết nối BottomNavigation với Navigation
+        NavigationUI.setupWithNavController(
+                bottomNavigationView, navController);
     }
 }
