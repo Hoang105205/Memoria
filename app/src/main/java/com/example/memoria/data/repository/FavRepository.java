64f7b441ca85ef99a7120451ccd04a8dd94cc9ec
Ruby <nhthuan23@clc.fitus.edu.vn>
2026-02-28 15:17:07 +0700

    feat: create UI for Library Page. Create logic and UI add, change name, delete a favorite folder. Create logic and UI add new deck

--- app/src/main/java/com/example/memoria/data/repository/FavRepository.java
+++ app/src/main/java/com/example/memoria/data/repository/FavRepository.java
@@ -1,4 +1,51 @@
 package com.example.memoria.data.repository;
 
+import android.app.Application;
+import com.example.memoria.data.database.AppDatabase;
+import com.example.memoria.data.database.dao.FavDao;
+import com.example.memoria.data.model.FavFolder;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class FavRepository {
+    private final FavDao favDao;
+    private final ExecutorService executor;
+
+    public FavRepository(Application application) {
+        AppDatabase db = AppDatabase.getDatabase(application);
+        favDao = db.favDao();
+        executor = Executors.newSingleThreadExecutor();
+    }
+
+    public interface DataCallback<T> {
+        void onDataLoaded(T data);
+    }
+
+    public void getAllFolders(DataCallback<List<FavFolder>> callback) {
+        executor.execute(() -> {
+            List<FavFolder> data = favDao.getAllFolders();
+            callback.onDataLoaded(data);
+        });
+    }
+
+    public void insertFolder(FavFolder folder) {
+        executor.execute(() -> favDao.insertFolder(folder));
+    }
+
+    public void getFolderById(UUID folderId, DataCallback<FavFolder> callback) {
+        executor.execute(() -> {
+            FavFolder folder = favDao.getFavFolderById(folderId);
+            callback.onDataLoaded(folder);
+        });
+    }
+
+    public void updateFolder(FavFolder folder) {
+        executor.execute(() -> favDao.updateFolder(folder)); // name only
+    }
+
+    public void deleteFolder(FavFolder folder) {
+        executor.execute(() -> favDao.deleteFolder(folder));
+    }
 }
--- app/src/main/java/com/example/memoria/ui/library/LibraryFragment.java
+++ app/src/main/java/com/example/memoria/ui/library/LibraryFragment.java
@@ -4,62 +4,135 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.memoria.R;
+import com.example.memoria.data.model.Deck;
+import com.example.memoria.data.model.FavFolder;
+import com.example.memoria.ui.adapter.CollectionAdapter;
+import com.example.memoria.ui.adapter.DeckAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LibraryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Date;
+import java.util.UUID;
+
 public class LibraryFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private LibraryViewModel viewModel;
+    private DeckAdapter deckAdapter;
+    private CollectionAdapter collectionAdapter;
+    private RecyclerView recyclerView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView tvTabCollections, tvTabDecks;
 
-    public LibraryFragment() {
-        // Required empty public constructor
-    }
+    private boolean isCollectionTabActive = true; // Trạng thái hiện tại: true = Tab Collections, false = Tab Decks
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LibraryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LibraryFragment newInstance(String param1, String param2) {
-        LibraryFragment fragment = new LibraryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_library, container, false);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewModel = new ViewModelProvider(this).get(LibraryViewModel.class);
+
+        // Ánh xạ View
+        recyclerView = view.findViewById(R.id.rv_library_list);
+        tvTabCollections = view.findViewById(R.id.tv_tab_collections);
+        tvTabDecks = view.findViewById(R.id.tv_tab_decks);
+        ImageButton btnAdd = view.findViewById(R.id.btn_add_deck);
+
+        // Setup Adapter
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // deck adapter
+        deckAdapter = new DeckAdapter();
+
+        // collection adapter
+        collectionAdapter = new CollectionAdapter(folder -> {
+            // Tạo Bundle chứa ID của folder
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("FOLDER_ID", folder.getFolderId());
+
+            // Chuyển sang màn hình FavDetailFragment và mang theo bundle
+            androidx.navigation.Navigation.findNavController(requireView())
+                    .navigate(R.id.favDetailFragment, bundle);
+        });
+
+        // Lắng nghe dữ liệu
+        viewModel.getDecks().observe(getViewLifecycleOwner(), decks -> {
+            deckAdapter.setDecks(decks);
+        });
+
+        viewModel.getCollections().observe(getViewLifecycleOwner(), folders -> {
+            collectionAdapter.setFolders(folders);
+        });
+
+        // Sự kiện Click khi chuyển Tabs chế độ
+        tvTabCollections.setOnClickListener(v -> switchTab(true));
+        tvTabDecks.setOnClickListener(v -> switchTab(false));
+
+        // Nút Thêm (Logic rẽ nhánh tTùy thuộc tab nào đang mở)
+        btnAdd.setOnClickListener(v -> {
+            if (isCollectionTabActive) {
+                // Tạo FavFolder mới (0 words)
+                FavFolder newFolder = new FavFolder();
+                newFolder.setFolderId(UUID.randomUUID());
+                newFolder.setFolderName("Collection " + System.currentTimeMillis());
+                newFolder.setCreatedAt(new Date());
+                viewModel.addNewCollection(newFolder);
+            } else {
+                // Tạo Deck mới (Empty deck)
+                Deck newDeck = new Deck();
+                newDeck.setDeckId(UUID.randomUUID());
+                newDeck.setDeckName("Deck " + System.currentTimeMillis());
+                newDeck.setCreatedAt(new Date());
+                viewModel.addNewDeck(newDeck);
+            }
+        });
+
+        // Mở app lên thì mặc định gọi tab Collection
+        switchTab(true);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Mỗi khi màn hình Library hiển thị lại, ép ViewModel lấy data mới nhất từ DB
+        if (viewModel != null) {
+            viewModel.loadDecks();
+            viewModel.loadCollections();
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false);
+    // Hàm đổi Tab
+    private void switchTab(boolean toCollection) {
+        isCollectionTabActive = toCollection;
+
+        if (toCollection) {
+            // Đổi nút bấm
+            tvTabCollections.setBackgroundResource(R.drawable.bg_rounded_white);
+            tvTabDecks.setBackground(null);
+
+            // Đổi Adapter của RecyclerView thành Collection
+            recyclerView.setAdapter(collectionAdapter);
+        } else {
+            // Đổi nút bấm
+            tvTabDecks.setBackgroundResource(R.drawable.bg_rounded_white);
+            tvTabCollections.setBackground(null);
+
+            // Đổi Adapter của RecyclerView thành Deck
+            recyclerView.setAdapter(deckAdapter);
+        }
     }
 }
